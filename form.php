5b326b09eec083e74615a33c0d4ad1b2e6aae7be
syavanaaldrey <42725325+syavanaaldrey@users.noreply.github.com>
2018-10-05 03:34:36 -0700

    Update form.php

--- form.php
+++ form.php
@@ -3,11 +3,36 @@
         <title></title>
     </head>
     <body>
-        <form action="submit.php" method="post">
-            Nama<input type="text" name="nama" id=""><br>
-            Nim<input type="text" name="nim" id=""><br>
-            Tanggal Lahir<input type="date" name="tgl_lahir" id=""><br>
-            <input type="submit" value="Kirim">
-        </form>
+        <form action="submit.php" method="POST">
+		<table>
+			<tr>
+				<td>
+					NIM
+				</td>
+				<td>
+					<input type="text" name="nim">
+				</td>
+			<tr>
+				<td>
+					
+					Nama
+				</td>
+				<td>
+					<input type="text" name="nama">
+				</td>
+			<tr>
+				<td>
+					Tanggal Lahir
+				</td>
+				<td>
+					<input type="date" name="tgl_lahir">
+				</td>
+			<tr>
+				<td>
+					<input type="submit" name="submit" value="Kirim">
+				</td>
+			</tr>
+		</table>
+	</form>
     </body>
 </html>
