9b43714db6ecab8ebbaf86fa43dc51a439bb1ee5
syavanaaldrey <42725325+syavanaaldrey@users.noreply.github.com>
2018-10-05 03:37:54 -0700

    Update list.php

--- list.php
+++ list.php
@@ -1,30 +1,39 @@
 <?php
-require_once("db.php");git 
+require_once("db.php");
 ?>
-<table border=1>
-    <thead>
-        <th>Nama</th>
-        <th>Nim</th>
-        <th>Tanggal Lahir</th>
-    </thead>
-    <tbody>
-<?php
-$sql    = "SELECT * FROM siswa";
-$result = mysqli_query($conn, $sql);
+<html>
+<body>
+<table border="1">
+		<tr>
+			<th>NIM</th>
+			<th>Nama</th>
+			<th>Tanggal Lahir</th>
+			<th>Action</th>
+		</tr>
+		<?php
+            $sql = "SELECT * FROM siswa";
 
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "<tr>";
-        echo "<td>" . $row["nama"]. "</td>"; 
-        echo "<td>" . $row["nim"]. "</td>";
-        echo "<td>" . $row["tgl_lahir"]. "</td>";
-        echo "</tr>";
-    }
-} else {
-    echo "0 results";
-}
-mysqli_close($conn);
+            $result = mysqli_query($conn, $sql);
+            $row = mysqli_num_rows($result);
+
+            if (mysqli_num_rows($result) > 0) {
+			while ($row = mysqli_fetch_assoc($result)) {
+				?>
+				<tr>
+				<td><?php echo $row['NIM']?></td>
+				<td><?php echo $row['Nama']?></td>
+				<td><?php echo $row['tgl_lahir']?></td>
+				<td><a href="delete.php?id=<?php echo $row['id']?>">Hapus</a> | <a href="update.php?id=<?php echo $row['id']?>">Update</a></td>
+				</tr>
+				<?php
+			}
+		}else {
+			echo "0 Result";
+		}
+		mysqli_close($conn);
+        ?>
 ?> 
-    </tbody>
 </table>
+</body>
+</html>
+<h3><a href="form.php">ISI FORM</a></h3>
